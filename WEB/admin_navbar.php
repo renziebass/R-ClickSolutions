0ae5087637d4b23247469df0817799d3523aa10e
R-Click_LAPTOP <rclicksolutions.ph@gmail.com>
2023-04-19 10:35:33 +0800

    s

--- WEB/admin_navbar.php
+++ WEB/admin_navbar.php
@@ -6,16 +6,16 @@
           <div class="collapse navbar-collapse d-flex flex-row-reverse" id="navbarNavDropdown">
             <ul class="navbar-nav">
               <li class="nav-item active">
-                <a class="nav-link font-weight-bold" href="admin_home.php">Dashboard<span class="sr-only">(current)</span></a>
+                <a class="nav-link font-weight-bold" href="https://rclickpos.com/WEB/admin_home.php">Dashboard<span class="sr-only">(current)</span></a>
               </li>
               <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle font-weight-bold" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
                   Transactions
                 </a>
                 <div class="dropdown-menu">
-                  <a class="dropdown-item" href="admin_recent.php">Recent</a>
-                  <a class="dropdown-item" href="admin_paid.php?date=<?php echo date("Y-m-d") ?>">Paid</a>
-                  <a class="dropdown-item" href="admin_unpaid.php?date=<?php echo date("Y-m-d") ?>">Unpaid</a>
+                  <a class="dropdown-item" href="https://rclickpos.com/WEB/admin_recent.php">Recent</a>
+                  <a class="dropdown-item" href="https://rclickpos.com/WEB/admin_paid.php?date=<?php echo date("Y-m-d") ?>">Paid</a>
+                  <a class="dropdown-item" href="https://rclickpos.com/WEB/admin_unpaid.php?date=<?php echo date("Y-m-d") ?>">Unpaid</a>
                 </div>
               </li>
               <li class="nav-item dropdown">
