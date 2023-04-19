0aae08a73052db09a37bdb352d32d2cbd425fdfe
R-Click_LAPTOP <rclicksolutions.ph@gmail.com>
2023-04-19 09:51:07 +0800

    s

--- WEB/config.php
+++ WEB/config.php
@@ -1,5 +1,5 @@
 <?php
-   define('HOST','https://auth-db445.hstgr.io/');
+   define('HOST','u186319490_kg_db');
    define('USER','u186319490_admin123');
    define('PASS','Kg9182022');
    define('DB','u186319490_kg_db');
