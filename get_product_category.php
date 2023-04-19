f8414e66383db8c923a935631d59b0ade9d19fbf
R-Click_LAPTOP <rclicksolutions.ph@gmail.com>
2023-04-19 10:27:33 +0800

    s

--- get_product_category.php
+++ get_product_category.php
@@ -1,6 +1,6 @@
 <?php 
 
-define('HOST','https://auth-db445.hstgr.io/');
+define('HOST','localhost');
 define('USER','u186319490_admin123');
 define('PASS','Kg9182022');
 define('DB','u186319490_kg_db');
