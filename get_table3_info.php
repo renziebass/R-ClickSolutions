83ae8fd266a1d9dab48174155b61bd0c058e3e21
R-Click_LAPTOP <rclicksolutions.ph@gmail.com>
2023-04-23 00:26:35 +0800

    fixed dashboard

--- get_table3_info.php
+++ get_table3_info.php
@@ -12,6 +12,7 @@ $sql="SELECT
 CONCAT(tb_products.mc_brand,' ',tb_products.mc_model,' ',tb_products.category) AS specification,
 tb_products.product_brand,
 tb_products.available
+FROM tb_products
 WHERE tb_products.available <= 5 AND tb_products.available
 NOT IN (SELECT tb_products.available FROM tb_products WHERE tb_products.available='0')";
 
