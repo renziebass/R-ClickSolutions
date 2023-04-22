bf9bf181d7a9fcfe2964d9fe35f8e19d2080d645
R-Click_LAPTOP <rclicksolutions.ph@gmail.com>
2023-04-23 00:20:02 +0800

    s

--- get_table3_info.php
+++ get_table3_info.php
@@ -8,26 +8,14 @@
 $con = mysqli_connect(HOST,USER,PASS,DB);
 
  
-$sql="SELECT
-CONCAT(tb_products.mc_brand,' ',tb_products.mc_model,' ',tb_products.category) AS specification,
-tb_products.product_brand,
-tb_products.available
-FROM tb_products
+$sql="SELECT * FROM tb_products
 WHERE tb_products.available <= 5 AND tb_products.available
 NOT IN (SELECT tb_products.available FROM tb_products WHERE tb_products.available='0')";
 
-$res = mysqli_query($con,$sql);
- 
-$result = array();
- 
-while($row = mysqli_fetch_array($res)){
-array_push($result,
-array('specification'=>$row[0]),
-array('product_brand'=>$row[1]),
-array('available'=>$row[2]));
+$result = mysqli_query($con,$sql);
+
+while(($row = mysqli_fetch_assoc($result)) == true){
+	$data[]=$row;
 }
- 
-echo json_encode(array("result"=>$result));
- 
-mysqli_close($con);
+echo json_encode($data);
 ?>
