7ee32fe877c0cf1318321453b2a232d30dc15f96
R-Click_LAPTOP <rclicksolutions.ph@gmail.com>
2023-04-22 23:59:17 +0800

    low on stock and out of stock on dashboard android

--- get_table3_info.php
+++ get_table3_info.php
@@ -8,9 +8,12 @@
 $con = mysqli_connect(HOST,USER,PASS,DB);
 
  
-$sql="SELECT * FROM tb_products
+$sql="SELECT
+CONCAT(tb_products.mc_brand,' ',tb_products.mc_model,' ',tb_products.category) AS specification,
+tb_products.product_brand
+FROM tb_products
 WHERE tb_products.available <= 5 AND tb_products.available
-NOT IN (SELECT tb_products.available FROM tb_products WHERE tb_products.available='0');";
+NOT IN (SELECT tb_products.available FROM tb_products WHERE tb_products.available='0')";
 
 $result = mysqli_query($con,$sql);
 
--- get_table4_info.php
+++ get_table4_info.php
@@ -8,7 +8,10 @@
 $con = mysqli_connect(HOST,USER,PASS,DB);
 
  
-$sql="SELECT * FROM tb_products
+$sql="SELECT
+CONCAT(tb_products.mc_brand,' ',tb_products.mc_model,' ',tb_products.category) AS specification,
+tb_products.product_brand
+FROM tb_products
 WHERE tb_products.available = 0";
 
 $result = mysqli_query($con,$sql);
