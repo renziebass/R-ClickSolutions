947bac8c25a0d546a12e05fc910bccc9e357949f
R-Click_LAPTOP <rclicksolutions.ph@gmail.com>
2023-04-23 00:17:13 +0800

    fixed dashboard android

--- get_table3_info.php
+++ get_table3_info.php
@@ -10,7 +10,8 @@ $con = mysqli_connect(HOST,USER,PASS,DB);
  
 $sql="SELECT
 CONCAT(tb_products.mc_brand,' ',tb_products.mc_model,' ',tb_products.category) AS specification,
-tb_products.product_brand
+tb_products.product_brand,
+tb_products.available
 FROM tb_products
 WHERE tb_products.available <= 5 AND tb_products.available
 NOT IN (SELECT tb_products.available FROM tb_products WHERE tb_products.available='0')";
@@ -23,7 +24,7 @@ while($row = mysqli_fetch_array($res)){
 array_push($result,
 array('specification'=>$row[0]),
 array('product_brand'=>$row[1]),
-array('iteavailablems'=>$row[2]));
+array('available'=>$row[2]));
 }
  
 echo json_encode(array("result"=>$result));
