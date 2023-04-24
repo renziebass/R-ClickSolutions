b76935355f81aca6d571551036d3a624eb100072
R-Click_LAPTOP <rclicksolutions.ph@gmail.com>
2023-04-24 17:14:58 +0800

    table 4

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -1,3 +1,53 @@
+<?php
+include('user_session.php');
+$sql1="SELECT
+SUM(tb_payments.total) AS paid
+FROM tb_transactions
+LEFT JOIN tb_payments ON tb_transactions.id=tb_payments.id
+WHERE tb_transactions.status='paid' AND tb_transactions.date='".date("Y-m-d")."'";
+$result1=mysqli_query($db,$sql1);
+$row1 = mysqli_fetch_assoc($result1);
+
+$sql2="SELECT
+SUM(tb_cart.price) AS unpaid
+FROM tb_transactions
+LEFT JOIN tb_cart ON tb_transactions.id=tb_cart.transaction_id
+WHERE tb_transactions.status='unpaid' AND tb_transactions.date='".date("Y-m-d")."'";
+$result2=mysqli_query($db,$sql2);
+$row2 = mysqli_fetch_assoc($result2);
+
+$currentsales=$row1['paid']+$row2['unpaid'];
+
+$sql3="SELECT
+COUNT(tb_transactions.id) as paidcustomers
+FROM tb_transactions
+WHERE tb_transactions.status='paid' AND tb_transactions.date='".date("Y-m-d")."'";
+$result3=mysqli_query($db,$sql3);
+$row3 = mysqli_fetch_assoc($result3);
+
+$sql4="SELECT
+COUNT(tb_transactions.id) as unpaidcustomers
+FROM tb_transactions
+WHERE tb_transactions.status='unpaid' AND tb_transactions.date='".date("Y-m-d")."'";
+$result4=mysqli_query($db,$sql4);
+$row4 = mysqli_fetch_assoc($result4);
+
+$currentcustomers=$row3['paidcustomers']+$row4['unpaidcustomers'];
+
+$sql5="SELECT
+tb_transactions.date,
+SUM(tb_payments.total) AS sales,
+SUM(tb_cart.price) AS unpaid
+FROM tb_transactions
+JOIN tb_payments ON tb_transactions.id=tb_payments.id
+JOIN tb_cart ON tb_transactions.id=tb_cart.transaction_id
+WHERE tb_transactions.date NOT IN (SELECT tb_transactions.date FROM tb_transactions WHERE tb_transactions.date='".date("Y-m-d")."')
+GROUP BY tb_transactions.date
+ORDER BY tb_transactions.date DESC
+LIMIT 5";
+$result5=mysqli_query($db,$sql5);
+
+?>
 <!DOCTYPE html>
 <html>
 
@@ -27,7 +77,7 @@
                     <li class="nav-item"><a class="nav-link" href="transactions.html"><i class="fas fa-user"></i><span>Transactions</span></a></li>
                     <li class="nav-item"><a class="nav-link" href="table.html"><i class="fas fa-table"></i><span>Products</span></a></li>
                     <li class="nav-item"><a class="nav-link" href="login.html"><i class="far fa-user-circle"></i><span>Suppliers</span></a></li>
-                    <li class="nav-item"><a class="nav-link" href="login.html"><i class="far fa-user-circle"></i><span>Logout   </span></a></li>
+                    <li class="nav-item"><a class="nav-link" href="login.html"><i class="far fa-user-circle"></i><span>Logout</span></a></li>
                     <li class="nav-item"></li>
                 </ul><span class="text-center d-flex d-lg-flex" style="font-size: 12px;margin: 0px;margin-top: 0px;margin-bottom: 0px;padding: 0px;padding-bottom: 0px;padding-top: 0px;">Copyright © Brand 2023</span>
                 <div class="text-center d-none d-md-inline"></div>
@@ -108,7 +158,50 @@
                     <div class="row">
                         <div class="col-lg-5 col-xl-4" style="height: 180px;width: 600px;">
                             <div class="card shadow mb-4" style="height: 165px;">
-                                <div class="card-body" style="height: 165px;width: auto;"></div>
+                                <div class="card-body" style="height: 165px;width: auto;">
+                                <table style="width:100%;">
+                
+                <thead>
+                <h5 class="text-center">Zero Stocks</h5>
+                  <tr>
+                    <th scope="col" style="padding-left: 10px; padding-right: 10px;">Product ID</th>
+                    <th scope="col" style="padding-left: 10px; padding-right: 10px;">Brand</th>
+                    <th scope="col" style="padding-left: 10px; padding-right: 10px;">Specification</th>
+                    <th scope="col" style="padding-left: 10px; padding-right: 10px;">Available</th>
+                  </tr>
+                </thead>
+                <tbody>
+                <?php
+                  $sql="SELECT
+                  tb_products.id,
+                  tb_products.product_brand,
+                  CONCAT(tb_products.category,'-',tb_products.mc_model) AS specification,
+                  tb_products.available
+                  FROM
+                  tb_products
+                  WHERE tb_products.available='0'
+                  LIMIT 3";
+                                                      
+                  $result = mysqli_query($db,$sql);
+      
+                  if (mysqli_num_rows($result) > 0) 
+                  {
+                  foreach($result as $items)
+                  {
+                ?>
+                      <tr>  
+                      <td style="padding-left: 10px; padding-right: 10px;"><?php echo $items['id']; ?></td>
+                      <td style="padding-left: 10px; padding-right: 10px;"><?php echo $items['product_brand']; ?></td>
+                      <td style="padding-left: 10px; padding-right: 10px;"><?php echo $items['specification']; ?></td>
+                      <td style="padding-left: 10px; padding-right: 10px;"><?php echo $items['available']; ?></td>
+                      </tr>
+                <?php
+                  } 
+                } 
+                  ?>
+                </tbody>
+              </table>
+                                </div>
                             </div>
                         </div>
                         <div class="col-lg-5 col-xl-4" style="height: 180px;width: 600px;">
