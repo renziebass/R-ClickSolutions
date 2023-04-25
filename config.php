8e1da348c05c8227091c47a667c3675489f2f8b2
R-Click_LAPTOP <rclicksolutions.ph@gmail.com>
2023-04-25 20:23:21 +0800

    deleting V1 KG POS

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-   define('HOST','localhost');
-   define('USER','u186319490_admin123');
-   define('PASS','Kg9182022');
-   define('DB','u186319490_kg_db');
-   $db = mysqli_connect(HOST,USER,PASS,DB);
-
-   
-?>
