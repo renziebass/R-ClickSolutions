d3969079717354372b27b27a2fcf22a58d3e998f
R-Click_LAPTOP <rclicksolutions.ph@gmail.com>
2023-04-25 20:29:58 +0800

    database migrated

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
    define('HOST','localhost');
-   define('USER','root');
-   define('PASS','');
-   define('DB','kg_db');
+   define('USER','u186319490_admin123');
+   define('PASS','Kg9182022');
+   define('DB','u186319490_kg_db');
    $db = mysqli_connect(HOST,USER,PASS,DB);
 ?>
