23b8d7f20305b6aebeba9a2e4d68da05460ee610
R-Click_LAPTOP <rclicksolutions.ph@gmail.com>
2023-04-28 20:53:30 +0800

    s

--- line.php
+++ line.php
@@ -3,10 +3,10 @@
           <div class="col">
             <div class="row border border-dark p-1">
               <div class="col-3 mx-auto py-auto">
-                  <img class="m-0" src="https://chart.googleapis.com/chart?chs=80x80&cht=qr&chl=<?php echo $row1['id']; ?>">
+                  <img class="m-0" src="https://chart.googleapis.com/chart?chs=100x100&cht=qr&chl=<?php echo $row1['id']; ?>">
               </div>
-              <div class="col-9">
-                  <p class="m-0 p-0" style="font-size: 12px;" ><?php echo $row1['category']; ?></p>
+              <div class="col-9 my-auto py-auto">
+                  <p class="m-0 p-0" style="font-size: 12px;" ><?php echo $row1['cpb']; ?></p>
                   <p class="m-0 p-0" style="font-size: 11px;"><?php echo $row1['specification']; ?></p>
                   <p class="m-0 p-0 text-danger" style="font-size: 12px; font-weight: bold;"><?php echo $row1['price']; ?></p>
               </div>
@@ -16,9 +16,9 @@
           <div class="col">
             <div class="row border border-dark p-1">
               <div class="col-3 mx-auto py-auto">
-                  <img class="m-0" src="https://chart.googleapis.com/chart?chs=80x80&cht=qr&chl=<?php echo $row1['id']; ?>">
+                  <img class="m-0" src="https://chart.googleapis.com/chart?chs=90x90&cht=qr&chl=<?php echo $row1['id']; ?>">
               </div>
-              <div class="col-9">
+              <div class="col-9 my-auto py-auto">
                   <p class="m-0 p-0" style="font-size: 12px;" ><?php echo $row1['cpb']; ?></p>
                   <p class="m-0 p-0" style="font-size: 11px;"><?php echo $row1['specification']; ?></p>
                   <p class="m-0 p-0 text-danger" style="font-size: 12px; font-weight: bold;"><?php echo $row1['price']; ?></p>
@@ -31,21 +31,8 @@
               <div class="col-3 mx-auto py-auto">
                   <img class="m-0" src="https://chart.googleapis.com/chart?chs=80x80&cht=qr&chl=<?php echo $row1['id']; ?>">
               </div>
-              <div class="col-9">
-                  <p class="m-0 p-0" style="font-size: 12px;" ><?php echo $row1['category']; ?></p>
-                  <p class="m-0 p-0" style="font-size: 11px;"><?php echo $row1['specification']; ?></p>
-                  <p class="m-0 p-0 text-danger" style="font-size: 12px; font-weight: bold;"><?php echo $row1['price']; ?></p>
-              </div>
-            </div>
-          </div>
-          
-          <div class="col">
-            <div class="row border border-dark p-1">
-              <div class="col-3 mx-auto py-auto">
-                  <img class="m-0" src="https://chart.googleapis.com/chart?chs=80x80&cht=qr&chl=<?php echo $row1['id']; ?>">
-              </div>
-              <div class="col-9">
-                  <p class="m-0 p-0" style="font-size: 12px;" ><?php echo $row1['category']; ?></p>
+              <div class="col-9 my-auto py-auto">
+                  <p class="m-0 p-0" style="font-size: 12px;" ><?php echo $row1['cpb']; ?></p>
                   <p class="m-0 p-0" style="font-size: 11px;"><?php echo $row1['specification']; ?></p>
                   <p class="m-0 p-0 text-danger" style="font-size: 12px; font-weight: bold;"><?php echo $row1['price']; ?></p>
               </div>
