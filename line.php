59b265e6ee6b7385abe4a0ac08dc9fc3c6a064c0
R-Click_LAPTOP <rclicksolutions.ph@gmail.com>
2023-04-28 20:49:32 +0800

    s

--- line.php
+++ line.php
@@ -39,5 +39,18 @@
             </div>
           </div>
           
+          <div class="col">
+            <div class="row border border-dark p-1">
+              <div class="col-3 mx-auto py-auto">
+                  <img class="m-0" src="https://chart.googleapis.com/chart?chs=80x80&cht=qr&chl=<?php echo $row1['id']; ?>">
+              </div>
+              <div class="col-9 my-auto py-auto">
+                  <p class="m-0 p-0" style="font-size: 12px;" ><?php echo $row1['category']; ?></p>
+                  <p class="m-0 p-0" style="font-size: 11px;"><?php echo $row1['specification']; ?></p>
+                  <p class="m-0 p-0 text-danger" style="font-size: 12px; font-weight: bold;"><?php echo $row1['price']; ?></p>
+              </div>
+            </div>
+          </div>
+          
           
     </div>
