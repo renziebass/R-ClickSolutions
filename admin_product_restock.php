67523f33c00591d7bc8bdda948b9db6a3845ed28
R-Click_LAPTOP <rclicksolutions.ph@gmail.com>
2023-04-28 16:17:41 +0800

    added code for re-stocking

--- admin_product_restock.php
+++ admin_product_restock.php
@@ -29,23 +29,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   } else {
     $qty = validateInput($_POST["qty"]);
   }
-  if (empty($_POST["pid"])) {
-    $pid_err = "* ";
-  } else {
-    $pid = validateInput($_POST["pid"]);
-  }
+ 
 
-  if(!empty($_POST["qty"]) && !empty($_POST["pid"]))
+  if(!empty($_POST["qty"]) && !empty($_GET["Pid"]))
   {
     try {
       
         $sql2 = "UPDATE tb_products
         SET tb_products.stocks=tb_products.stocks+'$qty',tb_products.available=tb_products.available+'$qty'
-        WHERE tb_products.id='$pid'";
+        WHERE tb_products.id='$_GET['Pid']'";
         $RestockUpdate = mysqli_query($db, $sql2);
 
         $sql3 = "INSERT INTO `tb_restock_history` (`product_id`, `date`, `qty`)
-        VALUES ('$pid', '".date("Y-m-d H:i:s")."', '$qty')";
+        VALUES ('$_GET['Pid']', '".date("Y-m-d H:i:s")."', '$qty')";
         $Restock = mysqli_query($db, $sql3);
         
         header("Location: admin_restock_product.php");
