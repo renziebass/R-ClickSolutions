d62ab4e7f54c220fe31ea20fe5ec34292bc5c943
R-Click_LAPTOP <rclicksolutions.ph@gmail.com>
2023-04-28 15:54:57 +0800

    button added outline

--- admin_product_restock.php
+++ admin_product_restock.php
@@ -110,7 +110,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                       <div class="input-group input-group-sm">
                                           <button class="btn btn-outline-secondary" type="submit">Scan</button>
                                           <div class="btn-group">
-                                              <button class="btn btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                              <button class="btn btn-outline-secondary btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                                                   Select Product
                                               </button>
                                               <ul class="dropdown-menu">
