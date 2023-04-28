678d9df43a9af6d479e0977ebd91e6b0390d3571
R-Click_LAPTOP <rclicksolutions.ph@gmail.com>
2023-04-28 21:00:35 +0800

    s

--- admin_product.php
+++ admin_product.php
@@ -76,7 +76,10 @@ $row1 = mysqli_fetch_assoc($result1);
                       <button type="button" onclick="window.open('product_qr.php?product_id=<?php echo $_GET['id']?>&line=1')" class="btn btn-secondary btn-sm">QR Code</button>
                     </div>
                     <div class="col-6 d-flex flex-row-reverse">
-                      <button type="button" onclick="printDiv();" class="btn btn-secondary btn-sm">Print</button>
+                      <button type="button" onclick="printDiv();" class="btn btn-secondary btn-sm"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-printer" viewBox="0 0 16 16">
+                      <path d="M2.5 8a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z"/>
+                      <path d="M5 1a2 2 0 0 0-2 2v2H2a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h1v1a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-1h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-1V3a2 2 0 0 0-2-2H5zM4 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2H4V3zm1 5a2 2 0 0 0-2 2v1H2a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1v-1a2 2 0 0 0-2-2H5zm7 2v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1z"/>
+                    </svg></button>
                       <script>
                           function printDiv() {
                           var printContents = document.getElementById("page").innerHTML;
