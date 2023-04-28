7a9c6417e490a4c7c8294653c4388a7e5a83a420
R-Click_LAPTOP <rclicksolutions.ph@gmail.com>
2023-04-28 18:25:54 +0800

    adding icons to dashboard

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -163,7 +163,7 @@ $row8 = mysqli_fetch_assoc($result8);
                                     <div class="row align-items-center no-gutters">
                                         <div class="col me-2">
                                             <div class="text-uppercase text-primary fw-bold text-xs mb-1"><span>SALES TODAY (<?php echo date("M d Y")?>)</span></div>
-                                            <div class="text-dark fw-bold h5 mb-0"><span><?php echo $row1['paid'];?></span></div>
+                                            <div class="text-dark fw-bold h5 mb-0"><span>P <?php echo $row1['paid'];?></span></div>
                                         </div>
                                         <div class="col-auto"><i class="fas fa-dollar-sign fa-2x text-gray-300"></i></div>
                                     </div>
@@ -176,7 +176,9 @@ $row8 = mysqli_fetch_assoc($result8);
                                     <div class="row align-items-center no-gutters">
                                         <div class="col me-2">
                                             <div class="text-uppercase text-primary fw-bold text-xs mb-1"><span>Customers</span></div>
-                                            <div class="text-dark fw-bold h5 mb-0"><span></span><?php echo $row3['paidcustomers'];?></div>
+                                            <div class="text-dark fw-bold h5 mb-0"><span></span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 16 16">
+                                              <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7Zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm-5.784 6A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216ZM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z"/>
+                                            </svg><?php echo $row3['paidcustomers'];?></div>
                                         </div>
                                         <div class="col me-2">
                                             <div class="text-uppercase text-primary fw-bold text-xs mb-1"><span>ITEMS</span></div>
