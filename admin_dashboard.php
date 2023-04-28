11f17875ef427d0ab266e97ace0f594d3697c416
R-Click_LAPTOP <rclicksolutions.ph@gmail.com>
2023-04-28 18:29:26 +0800

    added icons

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -178,11 +178,13 @@ $row8 = mysqli_fetch_assoc($result8);
                                             <div class="text-uppercase text-primary fw-bold text-xs mb-1"><span>Customers</span></div>
                                             <div class="text-dark fw-bold h5 mb-0"><span></span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 16 16">
                                               <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7Zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm-5.784 6A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216ZM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z"/>
-                                            </svg><?php echo $row3['paidcustomers'];?></div>
+                                            </svg> <?php echo $row3['paidcustomers'];?></div>
                                         </div>
                                         <div class="col me-2">
                                             <div class="text-uppercase text-primary fw-bold text-xs mb-1"><span>ITEMS</span></div>
-                                            <div class="text-dark fw-bold h5 mb-0"><span><?php echo $row6['items']; ?></span></div>
+                                            <div class="text-dark fw-bold h5 mb-0"><span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list-check" viewBox="0 0 16 16">
+                                              <path fill-rule="evenodd" d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3.854 2.146a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708L2 3.293l1.146-1.147a.5.5 0 0 1 .708 0zm0 4a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708L2 7.293l1.146-1.147a.5.5 0 0 1 .708 0zm0 4a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0z"/>
+                                            </svg> <?php echo $row6['items']; ?></span></div>
                                         </div>
                                     </div>
                                 </div>
