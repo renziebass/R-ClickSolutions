72e83e0fada41d9ec7ff65b9419eea7ee8cebd40
R-Click_LAPTOP <rclicksolutions.ph@gmail.com>
2023-04-28 18:19:01 +0800

    s

--- admin_products.php
+++ admin_products.php
@@ -97,7 +97,7 @@ $row4 = mysqli_fetch_assoc($result4);
                         </div>
                     </div>
                     <div class="col-6 d-flex justify-content-center">
-                    <div class="btn-group">
+                    <div class="btn">
                         <button class="btn btn-secondary btn-sm" type="button" data-bs-toggle="dropdown" aria-expanded="false"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
                           <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                           <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
