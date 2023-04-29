07da84053eb4b3333e3ab62591b437f29dbeed9c
R-Click_LAPTOP <rclicksolutions.ph@gmail.com>
2023-04-29 11:43:26 +0800

    fixing list of unpaid

--- get_table5_info.php
+++ get_table5_info.php
@@ -8,14 +8,20 @@
 $con = mysqli_connect(HOST,USER,PASS,DB);
 
  
-$sql="SELECT
-tb_transactions.id,
-tb_transactions.name,
-CONCAT(tb_transactions.date,' ',tb_transactions.time) AS date_time,
-SUM(tb_cart.total) AS amount
-FROM tb_transactions
-INNER JOIN tb_cart ON tb_transactions.id=tb_cart.transaction_id
-WHERE tb_transactions.status='unpaid'";
+$sql="SELECT *
+FROM (SELECT
+		tb_transactions.id,
+		tb_transactions.name,
+		tb_transactions.date
+		FROM tb_transactions WHERE tb_transactions.status='unpaid') AS A
+JOIN (SELECT
+		tb_cart.transaction_id,
+		SUM(tb_cart.quantity) as items,
+		SUM(tb_cart.price*tb_cart.quantity) AS total
+        FROM tb_cart
+		GROUP BY tb_cart.transaction_id) AS B
+ON A.id=B.transaction_id
+GROUP BY A.id";
 
 $result = mysqli_query($con,$sql);
 
