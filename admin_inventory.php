2eb6958c0de8c9cfae997ef655a96d657835165a
R-Click_LAPTOP <rclicksolutions.ph@gmail.com>
2023-05-01 18:11:48 +0800

    s

--- admin_inventory.php
+++ admin_inventory.php
@@ -81,7 +81,18 @@ $row3 = mysqli_fetch_assoc($result3);
             <div class="card-body">
             <div class="container text-center">
                 <div class="row align-items-start">
-                    <div class="col-12 d-flex flex-row-reverse">
+                    <div class="col-6">
+                      <div class="dropdown">
+                        <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                          All Inventory
+                        </button>
+                        <ul class="dropdown-menu">
+                          <li><a class="dropdown-item" href="#">Zero Stocks</a></li>
+                          <li><a class="dropdown-item" href="#">Low Stocks</a></li>
+                        </ul>
+                      </div>
+                    </div>
+                    <div class="col-6 d-flex flex-row-reverse">
                       <button type="button" onclick="printDiv();" class="btn btn-secondary btn-sm"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-printer" viewBox="0 0 16 16">
                         <path d="M2.5 8a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z"/>
                         <path d="M5 1a2 2 0 0 0-2 2v2H2a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h1v1a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-1h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-1V3a2 2 0 0 0-2-2H5zM4 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2H4V3zm1 5a2 2 0 0 0-2 2v1H2a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1v-1a2 2 0 0 0-2-2H5zm7 2v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1z"/>
