1e1fa02ff4e4d3be5aae409bc55bf6079491d7cb
R-Click_LAPTOP <rclicksolutions.ph@gmail.com>
2023-05-02 15:27:29 +0800

    s

--- get_table2_info.php
+++ get_table2_info.php
@@ -3,10 +3,11 @@ require('config_app.php');
 $except= $_GET['except'];
  
 $sql="SELECT *
+FROM (SELECT *
 FROM (SELECT
         tb_payments.date,
         COUNT(tb_payments.id) AS customers,
-	SUM(tb_payments.total) AS amount
+		SUM(tb_payments.total) AS amount
         FROM tb_transactions
         LEFT JOIN tb_payments ON tb_transactions.id=tb_payments.id
         WHERE tb_transactions.status='paid'
@@ -16,11 +17,13 @@ JOIN (SELECT
       tb_cart.date,
       DATE_FORMAT(tb_cart.date,'%M %d,%Y') AS date1,
     COUNT(tb_cart.transaction_id) as items
-     FROM tb_cart
-     WHERE tb_cart.date NOT IN ('$except')
-     GROUP BY tb_cart.date) AS B
+     FROM tb_transactions
+     JOIN tb_cart ON tb_transactions.id=tb_cart.transaction_id
+     WHERE tb_transactions.date NOT IN ('$except')
+     AND tb_transactions.status='paid'
+     GROUP BY tb_transactions.date) AS B
 ON A.date=B.date
-GROUP BY B.date DESC";
+GROUP BY B.date DESC;";
 
 $result = mysqli_query($con,$sql);
 
