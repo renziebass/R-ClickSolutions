fab50d4fdb49a521b41ee1a34d5d68c6c0451f81
R-Click_LAPTOP <rclicksolutions.ph@gmail.com>
2023-05-03 19:03:54 +0800

    s

--- get_table2_info.php
+++ get_table2_info.php
@@ -2,27 +2,20 @@
 require('config_app.php');
 $except= $_GET['except'];
  
-$sql="SELECT *
-FROM (SELECT
-        tb_payments.date,
-        COUNT(tb_payments.id) AS customers
-        FROM tb_transactions
-        LEFT JOIN tb_payments ON tb_transactions.id=tb_payments.id
-        WHERE tb_transactions.status='paid'
-        AND tb_payments.date NOT IN ('$except')
-        GROUP BY tb_transactions.date) AS A
-JOIN (SELECT
-      tb_cart.date,
-      CONCAT(FORMAT(SUM(tb_cart.price*tb_cart.quantity), 2)) AS amount,
-      DATE_FORMAT(tb_cart.date,'%M %d,%Y') AS date1,
-    COUNT(tb_cart.transaction_id) as items
-     FROM tb_transactions
-     JOIN tb_cart ON tb_transactions.id=tb_cart.transaction_id
-     WHERE tb_transactions.date NOT IN ('$except')
-     AND tb_transactions.status='paid'
-     GROUP BY tb_transactions.date) AS B
-ON A.date=B.date
-GROUP BY B.date DESC";
+$sql="SELECT * 
+FROM (SELECT DATE_FORMAT(tb_payments.date,'%M %d,%Y') AS date1,
+        COUNT(tb_payments.payment) AS customers,
+      CONCAT(FORMAT(SUM(tb_payments.total), 2)) AS amount
+        FROM tb_payments
+        WHERE tb_payments.date NOT IN ('$except')
+     	GROUP BY tb_payments.date) AS A
+JOIN (SELECT DATE_FORMAT(tb_payments.date,'%M %d,%Y') AS date1,
+     SUM(tb_cart.quantity) AS items
+     FROM tb_payments
+     JOIN tb_cart ON tb_payments.id=tb_cart.transaction_id
+     GROUP BY tb_payments.date DESC) AS B
+ON A.date1=B.date1
+GROUP BY A.date1 DESC";
 
 $result = mysqli_query($con,$sql);
 
