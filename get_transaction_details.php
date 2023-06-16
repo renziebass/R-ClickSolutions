e7da5b9157a60e9a873d89b4d53b3767f89971c5
R-Click_LAPTOP <rclicksolutions.ph@gmail.com>
2023-06-16 13:42:21 +0800

    s

--- get_transaction_details.php
+++ get_transaction_details.php
@@ -5,10 +5,12 @@ $transaction_id = $_GET['transaction_id'];
  
 $sql = "SELECT
 SUM(tb_cart.quantity) AS items,
-SUM(tb_cart.price*tb_cart.quantity) AS total
+SUM(tb_cart.price*tb_cart.quantity) AS total,
+tb_transactions.name
 FROM tb_cart
+JOIN tb_transactions ON tb_cart.transaction_id=tb_transactions.id
 WHERE tb_cart.transaction_id='$transaction_id'
-GROUP BY tb_cart.transaction_id;";
+GROUP BY tb_cart.transaction_id";
 
  
 $res = mysqli_query($con,$sql);
@@ -18,7 +20,8 @@ $result = array();
 while($row = mysqli_fetch_array($res)){
 array_push($result,
 array('items'=>$row[0]),
-array('total'=>$row[1]));
+array('total'=>$row[1]),
+array('name'=>$row[2]));
 }
  
 echo json_encode(array("result"=>$result));
