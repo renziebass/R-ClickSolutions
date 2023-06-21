d826db55669c6946839cabed8d4f6f0f6ab2c564
R-Click_LAPTOP <rclicksolutions.ph@gmail.com>
2023-06-21 20:54:42 +0800

    fixed error on previous income

--- get_table2_info.php
+++ get_table2_info.php
@@ -2,20 +2,22 @@
 require('config_app.php');
 $except= $_GET['except'];
  
-$sql="SELECT * 
-FROM (SELECT DATE_FORMAT(tb_payments.date,'%M %d,%Y') AS date1,
+$sql="SELECT *
+FROM (SELECT
+      	tb_payments.date,
+        DATE_FORMAT(tb_payments.date,'%M %d,%Y') AS date1,
         COUNT(tb_payments.payment) AS customers,
-      CONCAT(FORMAT(SUM(tb_payments.total), 2)) AS amount
+        CONCAT(FORMAT(SUM(tb_payments.total), 2)) AS amount
         FROM tb_payments
-        WHERE tb_payments.date NOT IN ('$except')
+      	WHERE tb_payments.date NOT IN ('$except')
      	GROUP BY tb_payments.date) AS A
-JOIN (SELECT DATE_FORMAT(tb_payments.date,'%M %d,%Y') AS date1,
-     SUM(tb_cart.quantity) AS items
-     FROM tb_payments
-     JOIN tb_cart ON tb_payments.id=tb_cart.transaction_id
-     GROUP BY tb_payments.date DESC) AS B
+JOIN (SELECT
+      SUM(tb_cart.quantity) AS items,
+      DATE_FORMAT(tb_cart.date,'%M %d,%Y') AS date1
+      FROM tb_cart
+      GROUP BY tb_cart.date) AS B
 ON A.date1=B.date1
-GROUP BY A.date1 DESC";
+ORDER BY A.date DESC";
 
 $result = mysqli_query($con,$sql);
 
