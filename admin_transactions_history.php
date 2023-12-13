a50f9fc0b12d87ee2463f1bc87904ac8177ab0b8
R-Click_LAPTOP <rclicksolutions.ph@gmail.com>
2023-12-13 11:11:45 +0800

    revert

--- admin_transactions_history.php
+++ admin_transactions_history.php
@@ -380,14 +380,13 @@ $row3 = mysqli_fetch_assoc($result3);
                 FROM (SELECT
                         tb_payments.date,
                         DATE_FORMAT(tb_payments.date,'%M %d,%Y') AS date1,
-                        COUNT(tb_payments.payment) AS customers,
-                        SUM(tb_payments.total) AS amount
+                        COUNT(tb_payments.payment) AS customers
                         FROM tb_payments
                         WHERE MONTH(tb_payments.date)='".$_GET['m']."' AND YEAR(tb_payments.date)='".$_GET['y']."'
                        GROUP BY tb_payments.date) AS A
                 JOIN (SELECT
                       SUM(tb_cart.quantity) AS items,
-                      CONCAT(FORMAT(SUM(tb_cart.price*tb_cart.quantity), 2)) AS amount2,
+                      CONCAT(FORMAT(SUM(tb_cart.price*tb_cart.quantity), 2)) AS amount,
                       DATE_FORMAT(tb_transactions.date,'%M %d,%Y') AS date1
                       FROM tb_transactions
                       JOIN tb_cart ON tb_transactions.id=tb_cart.transaction_id
