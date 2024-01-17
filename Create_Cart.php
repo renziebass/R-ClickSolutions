818db46269bef6102abe8d3358c33d12490cacce
R-Click_LAPTOP <rclicksolutions.ph@gmail.com>
2024-01-17 17:21:50 +0800

    fixed: add to cart

--- Create_Cart.php
+++ Create_Cart.php
@@ -1,9 +1,21 @@
 <?php
-require "DataBase.php";
-$db = new DataBase();
-if ($db->dbConnect()) {
-    if ($db->Create_Cart("tb_cart", $_POST['transaction_id'], $_POST['date'], $_POST['product_id'], $_POST['quantity'], $_POST['price'])){
-        echo "Added to Cart";
-    } else echo "Failed to Add Cart";
-} else echo "Error: Database connection";
+
+require('config_app.php');
+
+$transaction_id = $_POST['transaction_id'];
+$date = $_POST['date'];
+$product_id = $_POST['product_id'];
+$quantity = $_POST['quantity'];
+$price = $_POST['price'];
+$total = $price * $quantity;
+
+$sql1 = "INSERT INTO tb_cart (transaction_id, date, product_id,quantity,price,total) VALUES
+('$transaction_id','$date','$product_id','$quantity','$price','$total')";
+
+$sql2 ="UPDATE tb_products SET tb_products.available=tb_products.available-'$quantity'WHERE tb_products.id='$product_id'";
+
+
+if ((mysqli_query($con, $sql1)) && (mysqli_query($con, $sql2))) {
+    echo "Added to Cart";
+} else echo "Failed to Add Cart";
 ?>
