74e4efda305edb3efa0468e8bb28326238048ac5
R-Click_LAPTOP <rclicksolutions.ph@gmail.com>
2024-03-22 19:47:36 +0800

    new v

--- config_app.php
+++ config_app.php
@@ -1,12 +1,12 @@
 <?php
 define('HOST','localhost');
-define('USER','u186319490_admin123');
+define('USER','u186319490_renziebass');
 define('PASS','Kg9182022');
-define('DB','u186319490_kg_db');
+define('DB','u186319490_companies');
 
    
-$db = mysqli_connect(HOST,USER,PASS,DB);
-$con = mysqli_connect(HOST,USER,PASS,DB);
+$db1 = mysqli_connect(HOST,USER,PASS,DB);
+$con1 = mysqli_connect(HOST,USER,PASS,DB);
 
 date_default_timezone_set("Asia/Manila");
 
