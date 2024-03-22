b38a9a068861a747236b1189c420034267ad3ad6
R-Click_LAPTOP <rclicksolutions.ph@gmail.com>
2024-03-22 20:01:07 +0800

    back

--- config_app.php
+++ config_app.php
@@ -1,12 +1,12 @@
 <?php
 define('HOST','localhost');
-define('USER','u186319490_renziebass');
+define('USER','u186319490_admin123');
 define('PASS','Kg9182022');
-define('DB','u186319490_companies');
+define('DB','u186319490_kg_db');
 
    
-$db1 = mysqli_connect(HOST,USER,PASS,DB);
-$con1 = mysqli_connect(HOST,USER,PASS,DB);
+$db = mysqli_connect(HOST,USER,PASS,DB);
+$con = mysqli_connect(HOST,USER,PASS,DB);
 
 date_default_timezone_set("Asia/Manila");
 
--- user_session.php
+++ user_session.php
@@ -8,34 +8,10 @@
    FROM tb_accounts
    JOIN tb_users ON tb_accounts.userid=tb_users.userid
    WHERE tb_accounts.userid='$user_check'";
-   $result1 = mysqli_query($db1,$sql1);
+   $result1 = mysqli_query($db,$sql1);
    $row1 = mysqli_fetch_assoc($result1);
-
    $login_session = $row1['userid'];
    $name=$row1['name'];
-
-   /////////////////////////////////////////////////////////////////////
-
-   $sql2="SELECT
-   tb_accounts.user,
-   tb_accounts.pass,
-   tb_accounts.db
-   tb_users.company_id
-   FROM tb_accounts
-   LEFT JOIN tb_users
-   ON tb_accounts.id=tb_users.company_id
-   WHERE tb_users.user_id";
-   $result2 = mysqli_query($db,$sql2);
-   $row2 = mysqli_fetch_assoc($result2);
-
-
-   define('HOST1','localhost');
-   define('USER1',$row2['user']);
-   define('PASS1',$row1['pass']);
-   define('DB1',$row1['db']);
-
-   $db = mysqli_connect(HOST1,USER1,PASS1,DB1);
-   $con = mysqli_connect(HOST1,USER1,PASS1,DB1);
 
    if(!isset($_SESSION['id'])){
       header("location:index.php");
