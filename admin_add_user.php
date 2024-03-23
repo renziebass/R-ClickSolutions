6088787e1debc9233d279ec0708a9ef9aa6a4176
R-Click_LAPTOP <rclicksolutions.ph@gmail.com>
2024-03-23 23:31:53 +0800

    disabled save user

--- admin_add_user.php
+++ admin_add_user.php
@@ -425,7 +425,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             </div>
       </div>
       <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-            <button class="btn btn-success" type="submit">SAVE <span data-feather="upload-cloud" class="align-text-end"></button>
+            <button class="btn btn-success" type="submit" disabled>SAVE <span data-feather="upload-cloud" class="align-text-end"></button>
         </div>
     
       </form>
@@ -459,7 +459,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <td><?php echo "".$items['name'].""; ?></td>
                 <td>
         
-                  <button type="button" class="btn btn-sm p-0 m-0" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                  <button type="button" class="btn btn-sm p-0 m-0" data-bs-toggle="modal" data-bs-target="#exampleModal" disabled>
                     <span>
                       <svg  class="text-danger" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-square-fill" viewBox="0 0 16 16">
                       <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708z"/>
